8ba3f48ae9d9c3225d08dc8f9dc62ba8e867df8e
vinh <23127143@student.hcmus.edu.vn>
2025-09-15 00:04:12 +0700

    Change config

--- app/config.py
+++ app/config.py
@@ -1,12 +1,11 @@
 from pydantic_settings import BaseSettings
-from typing import List
+
 
 class Settings(BaseSettings):
-    secret_key: str = "change-me"
-    algorithm: str = "HS256"
-    access_token_expire_minutes: int = 60 * 24 * 7  # 7 ngày
-    sqlalchemy_database_url: str = "sqlite:///./dev.db"
-    cors_origins: List[str] = ["http://localhost:5173"]
+    DATABASE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
     class Config:
         env_file = ".env"
