a2569b864aef9b03c5bacf98743e192d3d85f46a
vinh <23127143@student.hcmus.edu.vn>
2025-09-15 02:53:32 +0700

    main.py

--- app/main.py
+++ app/main.py
@@ -21,7 +21,6 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    allow_credentials=False,
     allow_origin_regex=r"https://.*\.vercel\.app$"
 )
 
